258338e3b71e52fb12e2f280772b38d43011feb1
belle <ehoi.loveyourself@gmail.com>
2025-07-26 19:29:06 +0900

    remove: income, expense 관련 코드 전부 삭제

--- backend/src/main/java/com/bm/budget/repository/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bm.budget.repository;
-
-import com.bm.budget.domain.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
--- backend/src/main/java/com/bm/budget/repository/IncomeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bm.budget.repository;
-
-import com.bm.budget.domain.Expense;
-import com.bm.budget.domain.Income;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncomeRepository extends JpaRepository<Income, Long> {
-}
